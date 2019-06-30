d3a933fc3609319341beb5c790d958d3c04f8722
LiHongTai <631396529@qq.com>
2019-06-30 14:43:51 +0800

    Eureka Server 集群环境

---
+++ eureka-server-9001/src/main/java/github/roger/server/EurekaServer9001Application.java
@@ -0,0 +1,14 @@
+package github.roger.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer9001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer9001Application.class,args);
+    }
+}
